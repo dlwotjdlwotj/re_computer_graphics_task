a93f7017e8aeb286bc84b4e5d384857611414e11
이 재서 <dlwotjdlwotj@ajou.ac.kr>
2025-03-08 17:41:32 +0900

    make window

---
+++ Re_Computer_Graphics_Task/Re_Computer_Graphics_Task.cpp
@@ -0,0 +1,30 @@
+﻿#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+void render(GLFWwindow* window);
+
+int main(void) {
+	if (!glfwInit()) return -1;
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Hello", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	glewInit();
+
+	while (!glfwWindowShouldClose(window)) {
+		render(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+void render(GLFWwindow* window) {
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+	glClearColor(0, 0, .5, 0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glfwSwapBuffers(window);
+}
